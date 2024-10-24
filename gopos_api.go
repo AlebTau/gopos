dba4947b53fe3ef23397d073c91b917d440a55fd
aleb <bod.rooter@gmail.com>
2024-10-24 13:49:01 +0300

    Add func for getting orders from go POS api

--- gopos_api.go
+++ gopos_api.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"os"
 )
 
 // TokenResponse struct for unmarshalling the JSON response.
@@ -57,4 +58,54 @@ func GetTokenGoPos(goPosURL, clientID, clientSecret, email, password string) (st
 	}
 
 	return tokenResponse.AccessToken, nil
+}
+
+// GetOrders робить GET запит до API і зберігає результат у JSON файл
+func GetOrders(organizationID, page, dateStr, token, outputFilename string) error {
+	// URL для запиту
+	urlRequest := fmt.Sprintf(
+		"https://example.com/api/v3/%s/orders?include=table,employee,fiscalization,transactions,items,items.product,promotions,promotions.items&size=100&date_from=%s&page=%s",
+		organizationID, dateStr, page,
+	)
+
+	// Створення нового HTTP запиту
+	req, err := http.NewRequest("GET", urlRequest, nil)
+	if err != nil {
+		return fmt.Errorf("не вдалося створити запит: %v", err)
+	}
+
+	// Додавання заголовка авторизації
+	req.Header.Add("Authorization", "Bearer "+token)
+
+	// Виконання запиту
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("не вдалося виконати запит: %v", err)
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+
+		}
+	}(resp.Body)
+
+	// Перевірка статусу відповіді
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("помилка виконання запиту, код статусу: %d", resp.StatusCode)
+	}
+
+	// Зчитування тіла відповіді
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("не вдалося зчитати відповідь: %v", err)
+	}
+
+	// Збереження відповіді у JSON файл
+	err = os.WriteFile(outputFilename, body, 0644)
+	if err != nil {
+		return fmt.Errorf("не вдалося зберегти відповідь у файл: %v", err)
+	}
+
+	return nil
 }
